55cc89e15ea337c79742e0ef7238d38922e5207d
Gavri Kepets <gkepets@gmail.com>
2022-12-14 12:19:31 -0500

    fixed multipage issue

--- page_gen.py
+++ page_gen.py
@@ -4,16 +4,22 @@ import os
 from keyframe_extract import get_img_aesthetic_score
 import cv2
 
-def generate_page(video_name, max_width = 2):
-    page = PIL.Image.new(mode="RGB", size=(2400, 4000), color = "white")
+width = 2400
+height = 1000
 
+def generate_page(video_name, max_width = 2):
+    pages = []
+    pages.append(PIL.Image.new(mode="RGB", size=(width, height), color = "white"))
+
+    page_count = 0
     image_paths = os.listdir(video_name + "/speech_frames")
 
-    small_width = int((2400 - 200 - ((max_width - 1) * 50)) / max_width)
+    small_width = int((width - 200 - ((max_width - 1) * 50)) / max_width)
 
     row_count = 0
     column_count = 0
     page_height = 0
+    page = pages[0]
     for i, path in enumerate(image_paths):
         image_path = os.path.join(video_name, ("speech_frames/speechframe_" + str(i+1) + ".jpg"))
         image = Image.open(image_path)
@@ -26,7 +32,15 @@ def generate_page(video_name, max_width = 2):
         page.paste(image, (100 + row_count * small_width + 50*row_count, 100 + column_count * small_height + 50*column_count))
         row_count += 1
         page_height = 100 + column_count * small_height + 50*column_count + 100 + small_height
+        
+        if page_height + small_height + 150 > height and row_count == max_width:
+            page = page.crop((0, 0, page.width, page_height))
+            page_count += 1
+            pages.append(PIL.Image.new(mode="RGB", size=(width, height), color = "white"))
+            page = pages[page_count]
+            row_count = 0
+            column_count = 0
 
-    print(page_height)
-    page = page.crop((0, 0, page.width, page_height))
-    page.save('page.jpg')
+    
+    for i, page in enumerate(pages):
+        page.save('page'+str(i)+'.jpg')
